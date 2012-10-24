3543ecdc2e13bddf5a6f243436609bbe6c267d82
Tomas Hluchan <tomas.hluchan@gmail.com>
2012-10-24 20:02:54 +0200

    rand seed

--- index.py
+++ index.py
@@ -15,15 +15,25 @@ def myjoin(iterable):
 
 
 class MainPage(webapp2.RequestHandler):
-    def get(self):
-    
-        #r = random.Random(2)
-        r = random.Random()
+    def get(self, seed=None):
+        if seed is None:
+            return webapp2.redirect('/%d' % (random.randrange(0,100000)))
+
+        #if (mode is None) or not (mode in ['t','e','a']):
+        #    mode = 't'
+        #    return webapp2.redirect('/%s/%s' % (seed,mode))    
+
+        
+        mode='t'
+        r = random.Random(int(seed))
+        #r = random.Random()
 
         l1 = r.sample(string.ascii_uppercase,5)
         l2 = map(myjoin,r.sample(list(itertools.product(string.ascii_uppercase,repeat=2)),10))
         l = l1+l2
         r.shuffle(l)
+        if 'OK' in l:
+            l.remove('OK')
         l = r.sample(l,9)
         l3 = map(myjoin,r.sample(list(itertools.product(string.ascii_uppercase,repeat=3)),10))
         l.append('OK')
@@ -40,5 +50,8 @@ class MainPage(webapp2.RequestHandler):
         template = jinja_environment.get_template('index.html')
         self.response.out.write(template.render(template_values))
   
-app = webapp2.WSGIApplication([('/', MainPage)],
-                              debug=True)
+app = webapp2.WSGIApplication([
+    (r'/', MainPage),
+    (r'/(\d+)',MainPage),
+    ],
+    debug=True)
